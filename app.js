5ff6d5db652fc74c9204df32a3fa1bdcf3802ea3
jacoblovins <lovins.jacob@yahoo.com>
2020-07-11 08:16:44 -0400

    added and styled save buttons

--- app.js
+++ app.js
@@ -2,36 +2,23 @@ var currentDay = document.querySelector("#currentDay");
 var timeBlocks = document.querySelector(".container"); 
 var hourString = moment().format('H');
 var hourNum = parseInt(hourString);
-console.log(hourNum);
-console.log(typeof hourNum);
+// console.log(hourNum);
+// console.log(typeof hourNum);
 
+// Add current date to the top of the page
 currentDay.textContent = moment().format('dddd, MMM Do');
 
-// possible for each loop here
-// $(".row").each(function(){
-//     var blockHr = parseInt($(this).$(".col-md-10").attr("data-time"));
-//     console.log(blockHr);
-
-// })
-
+// Compare the current hour with each of the timeblock's hours
 $(".col-md-10").each(function(){
     var dataTime = $(this).attr("data-time");
     var dataNum = parseInt(dataTime);
-    console.log(dataNum);
+    // console.log(dataNum);
     if(hourNum < dataNum){
         $(this).addClass("future");
     }else if(hourNum === dataNum){
         $(this).addClass("present");
     }else{
         $(this).addClass("past");
-    }
-    
+    }  
 })
 
-// function timeColor(){
-//     if(){
-
-//     }else if() {
-
-//     }
-// }
