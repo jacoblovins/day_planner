3c3aae4604d9cc04a39eee5211f3ea7398cfce8f
jacoblovins <lovins.jacob@yahoo.com>
2020-07-15 16:01:40 -0400

    finished event storage functionality

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-var currentDay = document.querySelector("#currentDay");
-var timeBlocks = document.querySelector(".container"); 
 var hourString = moment().format('H');
 var hourNum = parseInt(hourString);
-// console.log(hourNum);
-// console.log(typeof hourNum);
 
 // Add current date to the top of the page
-currentDay.textContent = moment().format('dddd, MMM Do');
+$("#currentDay").text(moment().format('dddd, MMM Do'));
 
 // Compare the current hour with each of the timeblock's hours
 $(".col-md-10").each(function(){
-    var dataTime = $(this).attr("data-time");
+    var dataTime = $(this).attr("id");
     var dataNum = parseInt(dataTime);
-    // console.log(dataNum);
+    
+    // assign colors based on time
     if(hourNum < dataNum){
         $(this).addClass("future");
     }else if(hourNum === dataNum){
         $(this).addClass("present");
     }else{
         $(this).addClass("past");
-    }  
-})
+    } 
+});
 
+//Rendering stored inputs after page refresh
+function saveTodos(){
+    $(".todo").each(function(){
+        var inputId = $(this).attr("id");
+        $(this).val(localStorage.getItem(inputId));
+    });
+};
+
+//Click event to save user input in local storage
+$(".saveBtn").click(function(){
+    var scheduleInputs = $(this).siblings(".todo").val();
+    var inputsLocation = $(this).siblings(".todo").attr("id");
+    localStorage.setItem(inputsLocation,scheduleInputs);
+});
+
+saveTodos();
