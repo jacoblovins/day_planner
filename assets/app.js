deec5c6617b235330326a1aa2271467c83f262b2
jacoblovins <lovins.jacob@yahoo.com>
2020-07-16 13:32:44 -0400

    variable names more readable

--- assets/app.js
+++ assets/app.js
@@ -17,13 +17,13 @@ function hourLabel(i) {
 
 // Set time blocks
 for (var i = 9; i <= 17; i++) {
-    var row = $('<div class="row">');
-    var timeCol = $('<div class="col-md-1 hour">' + hourLabel(i) + '</div>');
-    var taskCol = $('<textarea class="todo col-md-9" id=' + i + '></textarea>');
-    var delCol = $('<button class="col-md-1 btn delBtn d-flex justify-content-center align-items-center"><i class="fas fa-trash-alt"></i></button>');
-    var saveCol = $('<button class="col-md-1 btn saveBtn d-flex justify-content-center align-items-center"><i class="fas fa-save"></i></button>');
-    row.append(timeCol, taskCol, delCol, saveCol);
-    $(".container").append(row);
+    var blockRow = $('<div class="row">');
+    var hour = $('<div class="col-md-1 hour">' + hourLabel(i) + '</div>');
+    var todoText = $('<textarea class="todo col-md-9" id=' + i + '></textarea>');
+    var delBtn = $('<button class="col-md-1 btn delBtn d-flex justify-content-center align-items-center"><i class="fas fa-trash-alt"></i></button>');
+    var saveBtn = $('<button class="col-md-1 btn saveBtn d-flex justify-content-center align-items-center"><i class="fas fa-save"></i></button>');
+    blockRow.append(hour, todoText, delBtn, saveBtn);
+    $(".container").append(blockRow);
     hourLabel(i);
     saveTodos();
 }
@@ -49,7 +49,7 @@ function saveTodos() {
         var inputId = $(this).attr("id");
         $(this).val(localStorage.getItem(inputId));
     });
-};
+}
 
 //Saving events into local storage
 $(".saveBtn").click(function () {
